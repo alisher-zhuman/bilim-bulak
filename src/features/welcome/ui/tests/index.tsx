ab6cf4b60eaa132eb6d402dd10bf28e10b76bd9b
Alisher <zumanovaliser814@gmail.com>
2026-01-16 22:13:42 +0600

    test ui

--- src/features/welcome/ui/tests/index.tsx
+++ src/features/welcome/ui/tests/index.tsx
@@ -1,3 +1,53 @@
+import { Button, Spinner } from "@heroui/react";
+import { useGetTests } from "@/entities/user/tests/model/api/queries";
+import { CircleQuestionMark, Clock } from "lucide-react";
+
 export const Tests = () => {
-  return <div>Tests</div>;
+  const { data: tests, isPending } = useGetTests();
+
+  if (isPending) {
+    return <Spinner />;
+  }
+
+  return (
+    <div>
+      <h2 className="font-bold text-2xl md:text-4xl text-center">
+        Сиз үчүн тесттер
+      </h2>
+
+      <div className="mt-14 flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap md:items-stretch gap-8 relative before:content-[''] before:absolute before:inset-0 before:-z-10 before:rounded-2xl before:bg-indigo-100 before:blur-2xl before:opacity-80">
+        <div className="bg-white rounded-3xl max-w-86.5 p-4 md:basis-[calc(50%-1rem)] lg:basis-0 lg:flex-1">
+          <h3 className="font-semibold text-xl md:text-2xl">
+            Психологиялык-эмоционалдык диагностика
+          </h3>
+
+          <p className="font-medium text-neutral-500 mt-2">
+            Тестти баштоо үчүн тынч жерге жайгашыңыз. Ортодон токтотууга
+            болбойт.
+          </p>
+
+          <div className="flex items-center gap-8 mt-6 font-medium">
+            <div className="flex items-center gap-1">
+              <Clock />
+              <span>30 мүнөт</span>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <CircleQuestionMark />
+              <span>20 суроо</span>
+            </div>
+          </div>
+
+          <p className="mt-6 font-medium text-neutral-500">
+            Баасы:
+            <span className="text-xl md:text-2xl text-blue-700"> 500с</span>
+          </p>
+
+          <Button className="bg-blue-700 mt-3 md:mt-6 rounded-xl w-full font-medium text-sm md:text-xl py-3.5 md:py-6">
+            Оплатить
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/widgets/landing/welcome/index.tsx
+++ src/widgets/landing/welcome/index.tsx
@@ -2,25 +2,26 @@
 import { useTranslations } from "next-intl";
 import { Button } from "@heroui/react";
 import { MoveDown } from "lucide-react";
-import { useGetProfile } from "@/entities/user/profile/model/api/queries";
+import { Tests } from "@/features/welcome/ui/tests";
 
 export const Welcome = () => {
   const t = useTranslations();
 
   return (
-    <h1 className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <h1 className="font-bold text-3xl md:text-5xl text-center">
         {t("welcome.title")}
       </h1>
 
-      <p className="text-center text-balance mt-3 text-sm md:text-xl font-medium text-neutral-500">
-        {" "}
+      <p className="text-center text-balance mt-3 max-w-225 text-sm md:text-xl font-medium text-neutral-500">
         {t("welcome.text")}
       </p>
 
-      <Button className="bg-blue-700 flex items-center gap-2 mt-10 rounded-xl font-medium text-sm md:text-xl py-3 px-4 md:py-4 md:px-5 h-fit w-fit">
+      <Button className="bg-blue-700 flex items-center gap-2 mt-10 rounded-xl font-medium text-sm md:text-xl py-3 px-4 md:py-4 md:px-5 h-fit w-fit mb-40">
         {t("common.takeTest")} <MoveDown size={24} />
       </Button>
-    </h1>
+
+      <Tests />
+    </div>
   );
 };
